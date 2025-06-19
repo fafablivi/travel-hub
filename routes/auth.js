3bbe731e1602633c9ef564ea098f0677416bef70
Mamefal <matouf94@gmail.com>
2025-06-19 15:25:56 +0200

    fix login

--- routes/auth.js
+++ routes/auth.js
@@ -12,7 +12,7 @@ router.post('/', async (req, res) => {
 
     const token = uuidv4();
     try {
-        await redis.setEx(session:${token}, 900, userId);
+        await redis.setEx(`session:${token}`, 900, userId);
         res.json({ token, expires_in: 900 });
     } catch (err) {
         res.status(500).json({ error: 'Erreur Redis', details: err.message });
