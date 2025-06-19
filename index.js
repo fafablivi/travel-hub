d51a3190e3a43e7edb25acc215d94dbd798b81e9
Mamefal <matouf94@gmail.com>
2025-06-19 15:24:10 +0200

    login

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const app = express();
 app.use(express.json());
 app.use('/offers', require('./routes/offers'));
 app.use('/reco', require('./routes/reco'));
+app.use('/login', require('./routes/auth'));
 
 app.listen(3000, () => console.log('API up on http://localhost:3000'));
---
+++ routes/auth.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const redis = require('../services/redisClient');
+const { v4: uuidv4 } = require('uuid');
+
+router.post('/', async (req, res) => {
+    const { userId } = req.body;
+
+    if (!userId) {
+        return res.status(400).json({ error: 'userId manquant' });
+    }
+
+    const token = uuidv4();
+    try {
+        await redis.setEx(session:${token}, 900, userId);
+        res.json({ token, expires_in: 900 });
+    } catch (err) {
+        res.status(500).json({ error: 'Erreur Redis', details: err.message });
+    }
+});
+
+module.exports = router;
